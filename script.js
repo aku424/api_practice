d3f8df805be0753d45e6ed93e1769f9e3c834253
aku424tt <61079024+aku424tt@users.noreply.github.com>
2020-12-01 09:29:16 +0900

    finished reractaring

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+// DOM要素の取得
 const btn = document.getElementById("add");
 const lists = document.getElementById("lists");
 
-async function listUsers(){
+// メソッド
+// user情報の取得
+async function getUsers(){
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
-    
-    
-    if(confirm("実行しますか？") === true){
-        
-        users.forEach(function(user) {
-            const list = document.createElement("li");
-            list.innerText = user.name;
-            lists.appendChild(list);
-            console.log(list);
-        });
-    }
-    
+    return users;
 }
+// user情報の追加
+function addList(user) {
+    const list = document.createElement("li");
+    list.innerText = user.name;
+    lists.appendChild(list);
+    console.log(list);
+}
+//user情報の表示
+async function listUsers(){
+    const users = await getUsers();
+    if(confirm("実行しますか？") === true){
+        users.forEach(addList);
+    }
+}
+//イベント
+addEventListener("load" ,listUsers);
 
 btn.addEventListener("click",listUsers);
-addEventListener("load" ,listUsers);
